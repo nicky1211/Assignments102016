4547746bce1ecd9384a56b89d682e9df467a1162
Manick Mehra <manickmehra2006@yahoo.com>
2016-10-08 17:14:35 +0530

    Update properties

--- assig2-gitData/properties.py
+++ assig2-gitData/properties.py
@@ -1,3 +1,7 @@
+"""This is a properties file"""
+
+######################### Properties for setup.py #################################################
+
 repo_path="/home/manick/devopsAssasination/assig2-gitData/repo"
 dataViz_repo_path="/home/manick/devopsAssasination/assig2-gitData/repo/dataViz/data-visualization"
 mm16_repo_path="/home/manick/devopsAssasination/assig2-gitData/repo/mm16/moderndatabases-monsoon16"
@@ -5,3 +9,28 @@ sap_repo_path="/home/manick/devopsAssasination/assig2-gitData/repo/sap/sap-efrei
 dataViz_output_file="/home/manick/devopsAssasination/assig2-gitData/data/dataViz_log.txt"
 mm16_output_file="/home/manick/devopsAssasination/assig2-gitData/data/mm16_log.txt"
 sap_output_file="/home/manick/devopsAssasination/assig2-gitData/data/sap_log.txt"
+
+###################################################################################################
+
+
+######################### Properties for fileOPeration.py #########################################
+
+
+
+###################################################################################################
+
+
+
+
+######################### Properties for frequency.py #############################################
+
+
+
+###################################################################################################
+
+
+######################### Properties for stats.py #############################################
+
+
+
+###################################################################################################
